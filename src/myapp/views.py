6d1ed33da715efd1d6989aeac36dc381092d5f26
constantin <constantin.mokin@gmail.com>
2019-05-16 07:27:12 +0300

    hw17

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        return HttpResponse('len is {}' .format(max(len(name))))
+    return HttpResponse('It is GET request')
