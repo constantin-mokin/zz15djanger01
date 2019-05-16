9278452a7fc43756ddda964e835e1cbc2b8d7072
constantin <constantin.mokin@gmail.com>
2019-05-16 07:39:58 +0300

    hw17

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def index1(request):
     if request.method == "POST":
         name = request.POST.get('name')
         return HttpResponse('len is {}' .format(max(len(name))))
     return HttpResponse('It is GET request')
+
+def index2(request):
+    if request.method == "POST":
+        name = request.POST.get('calendar')
+        # if name == 01.01.2019 :#####
+        return HttpResponse('Happy New Year')
+    else:
+        return HttpResponse()
